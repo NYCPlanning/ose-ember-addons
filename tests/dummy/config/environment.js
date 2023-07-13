8ce38870e042464f720f82836a2410a546db3558
Tyler Matteo <tylermatteo434@gmail.com>
2023-07-13 09:47:34 -0400

    Update to ember 4.12

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -8,6 +8,7 @@ module.exports = function (environment) {
     locationType: 'auto',
     host: 'http://localhost:4200',
     EmberENV: {
+      EXTEND_PROTOTYPES: false,
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
