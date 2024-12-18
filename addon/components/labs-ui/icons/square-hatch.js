5d60b4bcc320bba5a91cfd342cbfcc15bc4fc83d
tangoyankee <miller.tim108@gmail.com>
2024-12-18 15:07:53 -0500

    template square hatch

---
+++ addon/components/labs-ui/icons/square-hatch.js
@@ -0,0 +1,8 @@
+import Component from '@glimmer/component';
+
+export default class SquareHatchComponent extends Component {
+  height = 10;
+  width = 17;
+  viewBox = '0 0 17 10';
+  preserveAspectRatio = 'xMinYMid';
+}
---
+++ app/components/labs-ui/icons/square-hatch.js
@@ -0,0 +1,1 @@
+export { default } from '@nycplanning/ember/components/labs-ui/icons/square-hatch';
