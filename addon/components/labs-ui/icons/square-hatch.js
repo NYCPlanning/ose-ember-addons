33e86ed2d9cafc71a6878961979b816bff930741
tangoyankee <miller.tim108@gmail.com>
2024-12-20 10:49:22 -0500

    options

--- addon/components/labs-ui/icons/square-hatch.js
+++ addon/components/labs-ui/icons/square-hatch.js
@@ -1,8 +1,15 @@
 import Component from '@glimmer/component';
 
 export default class SquareHatchComponent extends Component {
-  height = 10;
-  width = 17;
-  viewBox = '0 0 17 10';
-  preserveAspectRatio = 'xMinYMid';
+  get backgroundColor() {
+    this.args.options.backgroundColor || "rgba(0, 197, 255, 0.2)";
+  }
+
+  get hatchColor() {
+    this.args.options.hatchColor || "rgba(0, 0, 0, 1)";
+  }
+
+  get hatchWidth() {
+    this.args.options.hatchWidth || "0.5"
+  }
 }
