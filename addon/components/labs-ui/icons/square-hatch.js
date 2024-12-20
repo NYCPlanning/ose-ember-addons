14f1b5294b734f40af706cb6976a7589bcaf9735
tangoyankee <miller.tim108@gmail.com>
2024-12-20 10:59:37 -0500

    return options

--- addon/components/labs-ui/icons/square-hatch.js
+++ addon/components/labs-ui/icons/square-hatch.js
@@ -1,15 +1,19 @@
 import Component from '@glimmer/component';
+import { htmlSafe } from '@ember/template';
 
 export default class SquareHatchComponent extends Component {
   get backgroundColor() {
-    this.args.options.backgroundColor || "rgba(0, 197, 255, 0.2)";
+    const { backgroundColor } = this.agrs.options;
+    return backgroundColor !== undefined ? htmlSafe(backgroundColor) :  "rgba(0, 197, 255, 0.2)";
   }
 
   get hatchColor() {
-    this.args.options.hatchColor || "rgba(0, 0, 0, 1)";
+    const { hatchColor } = this.agrs.options;
+    return hatchColor !== undefined ? htmlSafe(hatchColor) : "rgba(0, 0, 0, 1)";
   }
 
   get hatchWidth() {
-    this.args.options.hatchWidth || "0.5"
+    const { hatchWidth } = this.agrs.options;
+    return hatchWidth !== undefined ? htmlSafe(hatchWidth) : "0.5";
   }
 }
